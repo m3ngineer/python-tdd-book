eb520f8184e0f3e7995f1225cb7e5043a19e787f
Matthew Eng <mattheweng@matt.dhcp.nd.edu>
2018-12-14 10:06:56 -0500

    Refactor functional test to use function that checks for row in list table

--- functional_tests.py
+++ functional_tests.py
@@ -10,6 +10,11 @@ class  NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it(self):
         # User has heard about a new online to-do app. She goes to chekc out homepage
         self.browser.get('http://localhost:8000')
@@ -30,12 +35,17 @@ class  NewVisitorTest(unittest.TestCase):
         input_box.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows], f"New to-do item did not appear in table. Contents were: \n{table.text}")
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text box inviting user to add another item
         # User enters 'Use peacock feathers to make a fly'
+        input_box = self.browser.find_element_by_id('id_new_item')
+        input_box.send_keys('Use peacock feathers to make a fly')
+        input_box.send_keys(Keys.ENTER)
+
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
+        # User wonders whether the stie will remember her list, then she sees the site has generated a unique url for her
         self.fail('Finish the test.')
 
 if __name__ == '__main__':
